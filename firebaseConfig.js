40a17169cef3282668e2098e917c924ca38f8af8
enrrutador <60167925+enrrutador@users.noreply.github.com>
2024-08-24 18:03:53 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,12 @@
-<!-- 
+/*
   © [2024] [SYSMARKETHM]. Todos los derechos reservados.
   
   Este archivo es parte de [M-Escaner], propiedad de [SYSMARKETHM].
   
   El uso, distribución o reproducción no autorizados de este material están estrictamente prohibidos.
   Para obtener permiso para usar cualquier parte de este código, por favor contacta a [https://sysmarket-hm.web.app/].
--->
+*/
+
 // firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
